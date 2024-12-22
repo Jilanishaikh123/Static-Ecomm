2012a9baafb838faaf89eef27228adaa57f0e96c
Jilanishaikh123 <shaikhgulamjilani21@gmail.com>
2024-12-22 21:06:18 +0530

    Initial commit

---
+++ website.js
@@ -0,0 +1,234 @@
+let products = {
+  Electronics: [
+    {
+      name: "Inspiron 15",
+      price: 50000,
+      description:
+        "lksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksanco",
+      p_image:
+        "https://images-cdn.ubuy.co.in/633aae8db52ac20d0b37956e-dell-inspiron-15-3000-laptop-2021.jpg",
+    },
+    {
+      name: "Iphone",
+      price: 150000,
+      description:
+        "lksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksanco",
+      p_image:
+        "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202408/iphone-15-20251631-16x9_1.jpg?VersionId=5P_5gitDRCipZj0rMeEp2lIe85qeVBYK&size=690:388",
+    },
+    {
+      name: "M11+",
+      price: 13999,
+      description:
+        "lksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksanco",
+      p_image:
+        "https://images.samsung.com/is/image/samsung/levant-feature-an-easy-grip-design-in-an-array-of-stylish-colors-243829377?$FB_TYPE_A_MO_JPG$",
+    },
+  ],
+  Clothes: [
+    {
+      name: "MANIAC",
+      price: 1182,
+      description:
+        "lksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksanco",
+      p_image: "https://m.media-amazon.com/images/I/71Y8GOgzviL._SY741_.jpg",
+    },
+    {
+      name: "EYEBOGLER",
+      price: 299,
+      description:
+        "lksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksanco",
+      p_image:
+        "https://m.media-amazon.com/images/I/51kLcnRqvWL._AC_UY1100_.jpg",
+    },
+    {
+      name: "Jompers",
+      price: 935,
+      description:
+        "lksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksanco",
+      p_image:
+        "https://img.tatacliq.com/images/i8/437Wx649H/MP000000014063959_437Wx649H_202208061730351.jpeg",
+    },
+  ],
+  Footwear: [
+    {
+      name: "Slipper",
+      price: 200,
+      description:
+        "lksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksanco",
+      p_image: "https://m.media-amazon.com/images/I/61THqestSWL._SY625_.jpg",
+    },
+    {
+      name: "Sandals",
+      price: 500,
+      description:
+        "lksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksanco",
+      p_image:
+        "https://www.tracerindia.com/cdn/shop/files/05_52434a10-7518-434a-839c-607e5c7c4030.jpg?v=1688464798&width=823",
+    },
+    {
+      name: "Shoes",
+      price: 2000,
+      description:
+        "lksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksancolksamcoksanco",
+      p_image:
+        "https://m.media-amazon.com/images/I/41PuKqHnDqL._AC_UY1000_.jpg",
+    },
+  ],
+};
+
+// Declaring footer
+let footer = document.getElementById("footer");
+
+//This section is used to display category in the drop down list
+let category_selector = document.getElementById("category_selector");
+for (let category in products) {
+  let litag = document.createElement("li");
+  category_selector.append(litag);
+
+  let atag = document.createElement("a");
+  atag.setAttribute("class", "dropdown-item");
+  atag.innerText = category;
+  atag.addEventListener("click", () => display_selected_category(category));
+  litag.append(atag);
+}
+let litag = document.createElement("li");
+category_selector.append(litag);
+
+let atag = document.createElement("a");
+atag.setAttribute("class", "dropdown-item");
+atag.innerText = "All";
+atag.addEventListener("click", () => display_selected_category("All"));
+litag.append(atag);
+
+//function to display products of selected category
+function display_selected_category(category_name) {
+  if (document.querySelector(".container")) {
+    document.querySelector(".container").remove();
+  }
+  if (category_name == "All") {
+    display_products();
+  } else {
+    let container = document.createElement("div");
+    container.setAttribute("class", "container");
+
+    let row = document.createElement("div");
+    row.setAttribute("class", "row");
+    row.setAttribute("id", "product_row");
+
+    container.append(row);
+    let category_heading = document.createElement("h1");
+    category_heading.setAttribute("class", "category_heading");
+    category_heading.textContent = category_name;
+    row.append(category_heading);
+
+    products[category_name].forEach((element) => {
+      let column = document.createElement("div");
+      column.setAttribute("class", "col-4");
+      column.setAttribute("id", "product_col");
+      row.append(column);
+      let card = document.createElement("div");
+      card.setAttribute("class", "card");
+      card.style.width = "18rem";
+      card.style.height = "28rem";
+      column.append(card);
+
+      let img_container = document.createElement("div");
+      img_container.setAttribute("class", "img_container");
+      card.append(img_container);
+
+      let product_img = document.createElement("img");
+      product_img.setAttribute("class", "product_img");
+      product_img.setAttribute("src", element.p_image);
+      img_container.append(product_img);
+
+      let card_body = document.createElement("div");
+      card_body.setAttribute("class", "card-body");
+      card.append(card_body);
+      let card_title = document.createElement("h5");
+      card_title.setAttribute("class", "card-title");
+      card_title.textContent = element.name;
+      card_body.append(card_title);
+      let card_txt = document.createElement("p");
+      card_txt.setAttribute("class", "card-text");
+      card_txt.setAttribute("id", "card_txt");
+      card_txt.textContent = element.description;
+      card_body.append(card_txt);
+      let price = document.createElement("p");
+      price.setAttribute("class", "card-text");
+      price.textContent = `${element.price} Rs.`;
+      card_body.append(price);
+      let add_to_cart = document.createElement("a");
+      add_to_cart.setAttribute("class", "btn btn-primary");
+      add_to_cart.textContent = "Add to cart";
+      card_body.append(add_to_cart);
+    });
+
+    document.body.insertBefore(container, footer);
+  }
+}
+
+// This section is used to display products;
+function display_products() {
+  if (document.querySelector(".container")) {
+    document.querySelector(".container").remove();
+  }
+
+  let container = document.createElement("div");
+  container.setAttribute("class", "container");
+  for (let category in products) {
+    let row = document.createElement("div");
+    row.setAttribute("class", "row");
+    row.setAttribute("id", "product_row");
+
+    container.append(row);
+    let category_heading = document.createElement("h1");
+    category_heading.setAttribute("class", "category_heading");
+    category_heading.textContent = category;
+    row.append(category_heading);
+
+    products[category].forEach((element) => {
+      let column = document.createElement("div");
+      column.setAttribute("class", "col-4");
+      column.setAttribute("id", "product_col");
+      row.append(column);
+      let card = document.createElement("div");
+      card.setAttribute("class", "card");
+      card.style.width = "18rem";
+      card.style.height = "28rem";
+      column.append(card);
+
+      let img_container = document.createElement("div");
+      img_container.setAttribute("class", "img_container");
+      card.append(img_container);
+
+      let product_img = document.createElement("img");
+      product_img.setAttribute("class", "product_img");
+      product_img.setAttribute("src", element.p_image);
+      img_container.append(product_img);
+
+      let card_body = document.createElement("div");
+      card_body.setAttribute("class", "card-body");
+      card.append(card_body);
+      let card_title = document.createElement("h5");
+      card_title.setAttribute("class", "card-title");
+      card_title.textContent = element.name;
+      card_body.append(card_title);
+      let card_txt = document.createElement("p");
+      card_txt.setAttribute("class", "card-text");
+      card_txt.setAttribute("id", "card_txt");
+      card_txt.textContent = element.description;
+      card_body.append(card_txt);
+      let price = document.createElement("p");
+      price.setAttribute("class", "card-text");
+      price.textContent = `${element.price} Rs.`;
+      card_body.append(price);
+      let add_to_cart = document.createElement("a");
+      add_to_cart.setAttribute("class", "btn btn-primary");
+      add_to_cart.textContent = "Add to cart";
+      card_body.append(add_to_cart);
+    });
+  }
+
+  document.body.insertBefore(container, footer);
+}
